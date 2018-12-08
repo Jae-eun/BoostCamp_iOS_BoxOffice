ddc5f1f92167ad47184cb6397208215b50547517
Jae-eun <je4297@naver.com>
2018-12-08 20:10:01 +0900

    create cell & controller files

---
+++ Boostcamp_iOS_BoxOffice/View/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  Boostcamp_iOS_BoxOffice
+//
+//  Created by 이재은 on 08/12/2018.
+//  Copyright © 2018 이재은. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
