b84d914cb1f431b6a96e82dc1393271361fdf6ab
Jae-eun <je4297@naver.com>
2018-12-14 13:25:57 +0900

    create Comments

---
+++ Boostcamp_iOS_BoxOffice/Model/Comments.swift
@@ -0,0 +1,21 @@
+//
+//  Comments.swift
+//  Boostcamp_iOS_BoxOffice
+//
+//  Created by 이재은 on 14/12/2018.
+//  Copyright © 2018 이재은. All rights reserved.
+//
+
+import Foundation
+
+struct APIResponse3: Codable {
+    let comments: [Comments]
+}
+
+struct Comments: Codable {
+    let rating: Double
+    let timestamp: Double
+    let writer: String
+    let movie_id: String
+    let contents: String
+}
