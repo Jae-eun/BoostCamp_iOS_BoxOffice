d6cefc52a5e4db017d00c4b77460e760ef2ac3aa
Jae-eun <je4297@naver.com>
2018-12-16 21:17:26 +0900

    MovieDetailVC 수정

--- Boostcamp_iOS_BoxOffice/Controller/MovieDetailViewController.swift
+++ Boostcamp_iOS_BoxOffice/Controller/MovieDetailViewController.swift
@@ -12,38 +12,23 @@ class MovieDetailViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var comments: [Comments] = []
-    var cellIdentifier: String = "MovieInfoCell"
-    var movieId: String = ""
+    let cellIdentifiers: [String] = ["MovieInfoCell", "SynopsisCell", "DirectorCell", "CommentsCell"]
     
-    
+    var movieInfo: MovieInfo?
+    var comments: [Comments]?
+    var movieId: String?
+
     func numberOfSections(in tableView: UITableView) -> Int {
         return 4
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if cellIdentifier == "CommentCell" {
-            return comments.count
-        } else {
-            return 0
+        switch section {
+        case 0,1,2:
+            return 1
+        default:
+            return comments?.count ?? 0
         }
-    }
-    
-    //    let weak var userImage: UIImageView!
-    //    @IBOutlet weak var nickNameLabel: UILabel!
-    //    @IBOutlet weak var creationTimeLabel: UILabel!
-    //    @IBOutlet weak var commentLabel: UILabel!
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell: CommentsTableViewCell = tableView.dequeueReusableCell(withIdentifier: "CommentCell", for: indexPath) as? CommentsTableViewCell else { return UITableViewCell() }
-        
-        let comment: Comments = self.comments[indexPath.row]
-        
-        cell.nickNameLabel.text = comment.writer
-        cell.creationTimeLabel.text = String(comment.timestamp)
-        cell.commentLabel.text = comment.contents
-        
-        return cell
     }
     
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
@@ -53,9 +38,110 @@ class MovieDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(didReceiveMovieInfoNotification(_:)), name: DidReceiveMovieInfoNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(didReceiveCommentsNotification(_:)), name: DidReceiveCommentsNotification, object: nil)
+        
+        requestMovieInfo(id: movieId ?? " ")
+        requestComments(id: movieId ?? " ")
     }
 
+    @objc func didReceiveMovieInfoNotification(_ noti: Notification) {
+        guard let movieInfo: MovieInfo = noti.userInfo?["movieInfo"] as? MovieInfo else { return  }
+        self.movieInfo = movieInfo
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        }
+    }
+    
+    @objc func didReceiveCommentsNotification(_ noti: Notification) {
+        guard let comments: [Comments] = noti.userInfo?["comments"] as? [Comments] else { return  }
+        self.comments = comments
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        }
+    }
+}
 
+extension MovieDetailViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let section = indexPath.section
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifiers[section], for: indexPath)
+        
+        guard let movieInfo = movieInfo else { return UITableViewCell() }
+//        guard let comments = [Comments] else { return UITableViewCell() }
+        
+        switch section {
+        case 0:
+            guard let movieInfoCell = cell as? MovieInfoTableViewCell else {
+                return UITableViewCell()
+            }
+            movieInfoCell.movieTitleLabel.text = movieInfo.title
+            movieInfoCell.releaseDateLabel.text = movieInfo.date
+            movieInfoCell.detailLabel.text = movieInfo.GenreAndDurationText
+            movieInfoCell.reservationRateLabel.text = movieInfo.reservationRateText
+            movieInfoCell.userRatingLabel.text = "\(movieInfo.userRating)"
+            movieInfoCell.audienceLabel.text = "\(movieInfo.audience)"
+            DispatchQueue.global().async {
+                guard let imageURL: URL = URL(string: movieInfo.image) else { return }
+                guard let imageData: Data = try? Data(contentsOf: imageURL) else { return }
+                
+                DispatchQueue.main.async {
+                    if let index: IndexPath = tableView.indexPath(for: cell) {
+                        if index.row == indexPath.row {
+                            movieInfoCell.movieImageView.image = UIImage(data: imageData)
+                        }
+                    }
+                }
+            }
+
+        case 1:
+            guard let synopsisCell = cell as? SynopsisTableViewCell else {
+                return UITableViewCell()
+            }
+            synopsisCell.synopsisLabel.text = movieInfo.synopsis
+
+        case 2:
+            guard let directorCell = cell as? DirectorTableViewCell else {
+                return UITableViewCell()
+            }
+            directorCell.directorLabel.text = movieInfo.director
+            directorCell.actorLabel.text = movieInfo.actor
+            
+        case 3:
+            guard let commentsCell = cell as? CommentsTableViewCell else {
+                return UITableViewCell()
+            }
+            if let comments = comments {
+                if indexPath.row == 0 {
+                    commentsCell.commentTitleLabel.isHidden = false
+                    commentsCell.composeButton.isHidden = false
+                } else {
+                    commentsCell.commentTitleLabel.isHidden = true
+                    commentsCell.composeButton.isHidden = true
+                }
+                commentsCell.writerLabel.text = comments[indexPath.row].writer
+                commentsCell.timestampLabel.text = "\(comments[indexPath.row].timestamp)"
+                commentsCell.contentsLabel.text = comments[indexPath.row].contents
+            }
+            
+        default:
+            break
+        }
+        return cell
+    }
+}
+
+extension MovieDetailViewController: UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        switch indexPath.section {
+//        case 0:
+//            return 250
+//        default:
+//            return 150
+//        }
+//    }
 }
